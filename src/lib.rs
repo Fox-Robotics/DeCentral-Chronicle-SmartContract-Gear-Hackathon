7c0b2a7c2a865f389b53c2e4114266338e9e1e92
juansr 12 <suarezjuanalvaro@gmail.com>
2023-06-08 01:34:15 +0000

    reply prueba

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,6 @@ extern "C" fn init(){}
 #[no_mangle]
 extern "C" fn handle(){
     msg::reply(String::from("Hello People"),0).expect("Errro while trying to send reply");
+    let payload_string: String = msg::load().expect("Unable to decode `String`");
+    msg::reply(payload_string,0).expect("Errro while trying to send reply");
 }
