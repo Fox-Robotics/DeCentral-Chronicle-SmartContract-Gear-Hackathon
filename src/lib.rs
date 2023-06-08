d2d888d93d43addbdfaefcf37040901af8101b72
Andre Schiaffino <andre@DESKTOP-IK9SLO5.localdomain>
2023-06-08 01:25:58 -0700

    Add base NFT code

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,7 @@
 #![no_std]
-use gstd::{prelude::*,msg};
 
-#[no_mangle]
-extern "C" fn init(){}
+#[cfg(not(feature = "binary-vendor"))]
+mod contract;
 
-#[no_mangle]
-extern "C" fn handle(){
-    msg::reply(String::from("Hello People"),0).expect("Errro while trying to send reply");
-    let payload_string: String = msg::load().expect("Unable to decode `String`");
-    msg::reply(payload_string,0).expect("Errro while trying to send reply");
-}
+#[cfg(feature = "binary-vendor")]
+include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
